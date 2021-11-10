fd580f2e5ddc76654812f46a9363c30c8cf2d864
Alex Runyan <alex@runyan.co>
2021-11-10 17:31:04 -0500

    Added composer config

--- install.sh
+++ install.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+# Install oh my zsh
+if [ ! ~/.zshrc ]; then
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)";
+fi
+
+# Install homebrew
+if ! brew; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Symlink logs
+if [ ! -L ~/logs ]; then
+	if [ -d /usr/local/var/log ]; then
+		ln -s /usr/local/var/log ~/logs
+	fi
+fi
+
 # Check if the symlink to the vim 
 # config exists, if not, link is
 if [ ! -L ~/.vimrc ]; then
